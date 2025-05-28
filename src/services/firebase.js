63e1f052b31878711ae5fe6d88850d4e9e3cc47c
Nicole <nicolecoelho26@gmail.com>
2025-05-28 10:04:11 -0300

    Criei arquivo .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArIoWO-mdebEm8VkKXLl0cAykzBgwAOQs",
-  authDomain: "adivinhaceleb-react.firebaseapp.com",
-  projectId: "adivinhaceleb-react",
-  storageBucket: "adivinhaceleb-react.firebasestorage.com",
-  messagingSenderId: "395522725556",
-  appId: "1:395522725556:web:b164f006dfa4678da869e1",
-  measurementId: "G-91GFPZXGV8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
