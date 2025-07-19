b8ec598bdaac334f40cf765ad12911e424a61277
Nicole <nicolecoelho26@gmail.com>
2025-07-19 07:47:55 -0300

    Testar os emails

--- src/services/email.js
+++ src/services/email.js
@@ -1,14 +1,26 @@
 import emailjs from "@emailjs/browser";
 
+// Esses dados vêm do site do EmailJS
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-export const sendEmailToUser = (email, name) => {
-  const templateParams = {
-    user_name: name,
-    user_email: email,
-  };
+export const sendEmailToUser = async (userEmail, userName) => {
+  try {
+    const templateParams = {
+      user_email: userEmail,
+      user_name: userName,
+    };
 
-  return emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY);
+    const response = await emailjs.send(
+      SERVICE_ID,
+      TEMPLATE_ID,
+      templateParams,
+      PUBLIC_KEY
+    );
+
+    console.log("Email enviado com sucesso!", response.status, response.text);
+  } catch (error) {
+    console.error("Erro ao enviar email:", error);
+  }
 };
